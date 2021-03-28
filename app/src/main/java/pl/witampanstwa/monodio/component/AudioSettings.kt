8e7cba76e6657c5891acb044e839409dd0cc3133
Robert Dziubek <robert.dziubek5@gmail.com>
2021-03-28 16:30:34 +0200

    Restructure (#1)
    * Change the way settings requests are handled
    
    * Merge remote-tracking branch 'origin/master'

---
+++ app/src/main/java/pl/witampanstwa/monodio/component/AudioSettings.kt
@@ -0,0 +1,33 @@
+package pl.witampanstwa.monodio.component
+
+import android.util.Log
+import pl.witampanstwa.monodio.extension.ShellBoolean
+
+/**
+ * Acts as a buffer between the android framework and the application's logic.
+ *
+ * NOTE: Settings object parameter retrieval – not to do it all through a relatively slow
+ *  communication channel – is done dynamically as the once retrieved android framework's value
+ *  will not change throughout application lifetime; parameter setting is on the other hand done
+ *  directly, as there is no room for further optimisation.
+ */
+class AudioSettings(private val communicationChannel: Shell) {
+    var mono: Boolean =
+        Shell.parseToBoolean(communicationChannel.execute("settings get system master_mono"))
+        private set(value) {
+            communicationChannel.execute(
+                "settings put system master_mono ${ShellBoolean(value).value}",
+                false
+            )
+
+            // TODO: Add an additional (deferred, working in the background thread) check.
+            field = value
+        }
+
+    fun flipMono() {
+        Log.d("Commander", "flipping setting")
+
+        Log.d("Commander", "Currently at: ${this.mono}, going into: ${!this.mono}")
+        this.mono = !this.mono
+    }
+}
